c37f1744d84af8eac074006a27a72c3c9f265d88
Julian Lawrance <julian.lawrance@gmail.com>
2020-12-25 17:46:14 +0000

    Create class for the result of a protect action being applied

---
+++ BattleSystem/BattleSystem/Moves/Actions/Results/ProtectResult.cs
@@ -0,0 +1,13 @@
+﻿namespace BattleSystem.Moves.Actions.Results
+{
+    /// <summary>
+    /// Class for the result of a protect action being applied to a character.
+    /// </summary>
+    public class ProtectResult : IMoveActionResult
+    {
+        /// <summary>
+        /// Gets or sets whether the protect action was applied to the character.
+        /// </summary>
+        public bool Applied { get; set; }
+    }
+}
