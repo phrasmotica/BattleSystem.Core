2a72607114014943f0a4fb7a68596d4976c359e2
Julian Lawrance <julian.lawrance@gmail.com>
2020-12-29 18:08:11 +0000

    Create builder class for Items

---
+++ BattleSystem/BattleSystem/Items/ItemBuilder.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using BattleSystem.Stats;
+
+namespace BattleSystem.Items
+{
+    /// <summary>
+    /// Builder class for items.
+    /// </summary>
+    public class ItemBuilder
+    {
+        /// <summary>
+        /// The item to build.
+        /// </summary>
+        private readonly Item _item;
+
+        /// <summary>
+        /// Whether the built item has a name.
+        /// </summary>
+        private bool _hasName;
+
+        /// <summary>
+        /// Whether the built item has a description.
+        /// </summary>
+        private bool _hasDescription;
+
+        /// <summary>
+        /// Creates a new <see cref="ItemBuilder"/> instance.
+        /// </summary>
+        public ItemBuilder()
+        {
+            _item = new Item();
+        }
+
+        /// <summary>
+        /// Names the built item.
+        /// </summary>
+        /// <param name="name">The built item's name.</param>
+        public ItemBuilder Name(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Item name must be non-null and non-whitespace!", nameof(name));
+            }
+
+            _item.SetName(name);
+            _hasName = true;
+            return this;
+        }
+
+        /// <summary>
+        /// Describes the built item.
+        /// </summary>
+        /// <param name="description">The built item's description.</param>
+        public ItemBuilder Describe(string description)
+        {
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                throw new ArgumentException("Item description must be non-null and non-whitespace!", nameof(description));
+            }
+
+            _item.SetDescription(description);
+            _hasDescription = true;
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the given stats transform to the built item.
+        /// </summary>
+        /// <param name="transform">The stats transform for the built item.</param>
+        public ItemBuilder WithStatsTransform(Func<StatSet, StatSet> transform)
+        {
+            _item.AddStatsTransform(transform);
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a stats transform for increasing attack by 10% to the built item.
+        /// </summary>
+        public ItemBuilder WithIncreaseAttack()
+        {
+            return WithStatsTransform(ss =>
+            {
+                ss.Attack.BaseValue = (int) (ss.Attack.BaseValue * 1.1);
+                return ss;
+            });
+        }
+
+        /// <summary>
+        /// Returns the built item.
+        /// </summary>
+        public Item Build()
+        {
+            if (!_hasName)
+            {
+                throw new InvalidOperationException("Cannot build an item with no name set!");
+            }
+
+            if (!_hasDescription)
+            {
+                throw new InvalidOperationException("Cannot build an item with no description set!");
+            }
+
+            return _item;
+        }
+    }
+}
