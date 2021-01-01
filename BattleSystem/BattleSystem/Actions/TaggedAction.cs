a59239eb2062016c66266d4033c258ac6c999411
Julian Lawrance <julian.lawrance@gmail.com>
2021-01-01 13:05:51 +0000

    Create class containing an IAction and an associated set of string tags

---
+++ BattleSystem/BattleSystem/Actions/TaggedAction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace BattleSystem.Actions
+{
+    /// <summary>
+    /// Represents an action associated with a set of tags.
+    /// </summary>
+    public class TaggedAction
+    {
+        /// <summary>
+        /// Gets or sets the underlying action.
+        /// </summary>
+        public IAction Action { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the set of tags for this action.
+        /// </summary>
+        public HashSet<string> Tags { get; private set; }
+
+        /// <summary>
+        /// Creates a new <see cref="TaggedAction"/> instance.
+        /// </summary>
+        public TaggedAction(IAction action, string[] tags)
+        {
+            Action = action;
+
+            Tags = new HashSet<string>();
+            foreach (var tag in tags)
+            {
+                Tags.Add(tag);
+            }
+        }
+
+        /// <summary>
+        /// Returns whether the action has the given tag.
+        /// </summary>
+        /// <param name="tag">The tag.</param>
+        public bool HasTag(string tag)
+        {
+            return Tags.Contains(tag);
+        }
+    }
+}
