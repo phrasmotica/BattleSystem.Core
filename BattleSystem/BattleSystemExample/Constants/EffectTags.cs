14169b564985ec998b24a02f5baa5d6532ca4263
Julian Lawrance <julian.lawrance@gmail.com>
2021-01-01 13:11:49 +0000

    Create static class containing tags to be attached to tagged actions

---
+++ BattleSystem/BattleSystemExample/Constants/EffectTags.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleSystemExample.Constants
+{
+    /// <summary>
+    /// Constants class for tags to be attached to effects.
+    /// </summary>
+    public static class EffectTags
+    {
+        public const string EndTurn = "EndTurn";
+    }
+}
