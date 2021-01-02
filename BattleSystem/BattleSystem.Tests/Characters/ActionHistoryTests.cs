55976938d93a9902eaddf96fe91a67068229ce36
Julian Lawrance <julian.lawrance@gmail.com>
2021-01-02 18:39:44 +0000

    Create unit tests for ActionHistory class

---
+++ BattleSystem/BattleSystem.Tests/Characters/ActionHistoryTests.cs
@@ -0,0 +1,41 @@
+﻿using BattleSystem.Actions.Results;
+using BattleSystem.Characters;
+using BattleSystem.Items;
+using BattleSystem.Moves;
+using NUnit.Framework;
+
+namespace BattleSystem.Tests.Characters
+{
+    /// <summary>
+    /// Unit tests for <see cref="ActionHistory"/>.
+    /// </summary>
+    [TestFixture]
+    public class ActionHistoryTests
+    {
+        [Test]
+        public void AddAction_ActionFromMove_AddedSuccessfully()
+        {
+            // Arrange
+            var history = new ActionHistory();
+
+            // Act
+            history.AddAction(new DamageResult<Move>());
+
+            // Assert
+            Assert.That(history.MoveActions.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddAction_ActionFromItem_AddedSuccessfully()
+        {
+            // Arrange
+            var history = new ActionHistory();
+
+            // Act
+            history.AddAction(new DamageResult<Item>());
+
+            // Assert
+            Assert.That(history.ItemActions.Count, Is.EqualTo(1));
+        }
+    }
+}
