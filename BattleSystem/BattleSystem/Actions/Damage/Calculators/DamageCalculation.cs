5cffd931660c7043b974de9945c32bac50cbe4e2
Julian Lawrance <julian.lawrance@gmail.com>
2021-01-05 17:47:50 +0000

    Create class representing the result of a damage calculation

---
+++ BattleSystem/BattleSystem/Actions/Damage/Calculators/DamageCalculation.cs
@@ -0,0 +1,18 @@
+﻿namespace BattleSystem.Actions.Damage.Calculators
+{
+    /// <summary>
+    /// Represents the result of a damage calculation.
+    /// </summary>
+    public class DamageCalculation
+    {
+        /// <summary>
+        /// Gets or sets whether the damage calculation was successful.
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Gets or sets the calculated amount of damage.
+        /// </summary>
+        public int Amount { get; set; }
+    }
+}
