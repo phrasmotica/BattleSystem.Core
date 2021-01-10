c8c3e4a410346169756be0550ff56eb0e5c1acc1
Julian Lawrance <julian.lawrance@gmail.com>
2021-01-10 17:20:23 +0000

    Create generic Slot class for ItemSlot to inherit from

--- BattleSystem.Core/Items/ItemSlot.cs
+++ BattleSystem.Core/Items/ItemSlot.cs
@@ -1,50 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using BattleSystem.Core.Characters;
 
 namespace BattleSystem.Core.Items
 {
     /// <summary>
     /// Represents a character's item slot.
     /// </summary>
-    public class ItemSlot
+    public class ItemSlot : Slot<Item>
     {
-        /// <summary>
-        /// The items that have occupied this item slot.
-        /// </summary>
-        private readonly Stack<Item> Items;
-
         /// <summary>
         /// Gets whether there is an active item in this item slot.
         /// </summary>
-        public bool HasItem => Items.Count > 0 && Current != null;
-
-        /// <summary>
-        /// Gets the current item.
-        /// </summary>
-        public Item Current => Items.Peek();
-
-        /// <summary>
-        /// Creates a new <see cref="ItemSlot"/> instance.
-        /// </summary>
-        public ItemSlot()
-        {
-            Items = new Stack<Item>();
-        }
-
-        /// <summary>
-        /// Sets the current item to the given item.
-        /// </summary>
-        /// <param name="item">The item.</param>
-        public void Set(Item item)
-        {
-            Items.Push(item);
-        }
-
-        /// <summary>
-        /// Removes the current item.
-        /// </summary>
-        public void Remove()
-        {
-            Items.Push(null);
-        }
+        public bool HasItem => HasAsset;
     }
 }
