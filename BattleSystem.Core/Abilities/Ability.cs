374383f269fb3336d4cbd9467764fe00d0a128f0
Julian Lawrance <julian.lawrance@gmail.com>
2021-01-10 17:20:53 +0000

    Create classes for a character's ability slot and the abilities it will contain

---
+++ BattleSystem.Core/Abilities/Ability.cs
@@ -0,0 +1,25 @@
+﻿using BattleSystem.Core.Actions;
+
+namespace BattleSystem.Core.Abilities
+{
+    /// <summary>
+    /// Represents an ability that a character possesses.
+    /// </summary>
+    public class Ability
+    {
+        /// <summary>
+        /// Gets or sets the name of the ability.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of the ability.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets this item's action container.
+        /// </summary>
+        public ActionContainer ActionContainer { get; } = new ActionContainer();
+    }
+}
---
+++ BattleSystem.Core/Abilities/AbilitySlot.cs
@@ -0,0 +1,15 @@
+﻿using BattleSystem.Core.Characters;
+
+namespace BattleSystem.Core.Abilities
+{
+    /// <summary>
+    /// Represents a character's ability slot.
+    /// </summary>
+    public class AbilitySlot : Slot<Ability>
+    {
+        /// <summary>
+        /// Gets whether there is an active ability in this ability slot.
+        /// </summary>
+        public bool HasAbility => HasAsset;
+    }
+}
